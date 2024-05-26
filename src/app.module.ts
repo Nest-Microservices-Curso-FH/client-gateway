4dc2100aeb6a1b59d37e23628bf33ed1c31ffc2d
tomasrpita <tomasrpita@gmail.com>
2024-05-26 08:20:43 +0200

    feat: switch client-gateway communication from TCP to NATS for products

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule],
+  imports: [
+    ProductsModule, 
+    OrdersModule
+  ],
 })
 export class AppModule {}
