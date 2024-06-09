2a0a10f236ad7c3f799b53344bd98f5f4e7884d5
tomasrpita <tomasrpita@gmail.com>
2024-06-09 09:41:24 +0200

    chore: Update AuthController with NATS communication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser() {}
+  registerUser() {
+    return this.client.send('auth.register.user', {})
+    }
+    
+    @Post('login')
+    loginUser() {
+    return this.client.send('auth.login.user', {})
+    
+    }
+    
+    @Get('verify')
+    verifyToken() {
+      return this.client.send('auth.verify.user', {})
 
-  @Post()
-  loginUser() {}
-
-  @Get('verify')
-  registerUser() {}
-
+    }
+    
 }
